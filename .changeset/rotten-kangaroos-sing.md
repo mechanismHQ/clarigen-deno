4904ab949d74f7d16a432e4687aafdba65a95b92
Hank Stoever <hstove@gmail.com>
2023-03-10 09:20:47 -0800

    fix: init command

---
+++ .changeset/rotten-kangaroos-sing.md
@@ -0,0 +1,5 @@
+---
+"clarigen-deno": patch
+---
+
+Fixes `clarigen init` command due to .toml files not being included from deno.land package manager
--- src/cli/commands/init.ts
+++ src/cli/commands/init.ts
@@ -1,21 +1,14 @@
-import { resolve } from '../../deps.ts';
 import { Command } from '../cli-deps.ts';
 import { cwdRelative, fileExists } from '../cli-utils.ts';
 import { CONFIG_FILE, configFilePath } from '../config.ts';
 import { log } from '../logger.ts';
-
-async function getBaseFile() {
-  const __dirname = new URL('.', import.meta.url).pathname;
-  const path = resolve(__dirname, '../files/init-config.toml');
-  const contents = await Deno.readTextFile(path);
-  return contents;
-}
+import { tomlInit } from '../files/init-config.ts';
 
 export const initCommand = new Command()
   .description('Initialize a Clarigen config file')
   .action(async () => {
     const path = configFilePath();
-    const baseFile = await getBaseFile();
+    const baseFile = tomlInit;
 
     if (await fileExists(path)) {
       const overwrite = confirm(`${CONFIG_FILE} already exists. Overwrite?`);
